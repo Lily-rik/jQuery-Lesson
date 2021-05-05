53e33fdcfda27a38e193e139641c5f72fe173c2b
reiko-ishihara <violet794.py16@gmail.com>
2021-05-05 09:15:25 +0900

    [anser]6-5

--- app3.js
+++ app3.js
@@ -1,7 +1,9 @@
 $(function(){
-  $('.box1').slideDown();
-  $('.box1').css({
-    'background-color': '#0000FF',
-    'height': '100px'
+  $('.box1').slideDown(function(){     // jsでは引数の中に関数を入れることができる
+    $('.box1').css({                   // slideDown()の引数にfunctionを書き加えることで、
+      'background-color': '#0000FF',   // スライドダウンした後にfunctionを実行することができるようになる。
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
   });
 });
